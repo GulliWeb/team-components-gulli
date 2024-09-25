cc3aa55f228650fc56715d60b88c9284c791a05c
Andrea Gulli <andreagulli11@gmail.com>
2024-09-25 20:43:34 +0200

    improved iframe extension when clicked

--- src/components/IframeVideos.vue
+++ src/components/IframeVideos.vue
@@ -3,19 +3,25 @@ export default {
   data() {
     return {
         showVideo: false,
-        cover: true
+        cover: true,
     }
   },
   methods:{
     playVideo(){
         this.showVideo = true 
         this.cover = false
+        const iframe = document.querySelector('.iframe-wrapper') 
+        iframe.classList.add('iframe-extend') // aggiungo la classe per estendere l'iframe una volta cliccato
+        document.body.classList.add('bg-black')
     },
     hideVideo(){
         this.showVideo = false
         this.cover = true
-    }
-  }
+        const iframe = document.querySelector('.iframe-wrapper') 
+        document.body.classList.remove('bg-black')
+        iframe.classList.remove('iframe-extend') // aggiungo la classe per rimuovere l'iframe una volta cliccato        
+    },
+  },
 }
 </script>
 
@@ -24,8 +30,8 @@ export default {
     <div class="iframe-wrapper">
         <!-- Contenitore per gestire effetti ed animazione su immagine di cover e di bottone YT -->
         <div class="overlay">
-            <img id="cover-image" v-if="this.cover" @click="playVideo" src="https://maxcoach-4437.kxcdn.com/main/wp-content/uploads/sites/1/2019/12/home-2-popup-video-poster.jpg" alt="">
-            <div class="yt-button" v-if="this.cover" @click="playVideo">
+            <img id="cover-image" v-if="cover" @click="playVideo" src="https://maxcoach-4437.kxcdn.com/main/wp-content/uploads/sites/1/2019/12/home-2-popup-video-poster.jpg" alt="">
+            <div class="yt-button" v-if="cover" @click="playVideo">
                 <img src="https://maxcoach-4437.kxcdn.com/main/wp-content/uploads/sites/1/2019/11/icon-youtube-play.png" alt="">
             </div>
         </div>
@@ -45,13 +51,19 @@ export default {
 // IFRAME 
 .iframe-wrapper{
     height: 450px;
-    width: 670px;
+    width: 100%;
     overflow: hidden; //Aggiungo l'overlow in modo che venga impedito all'immgaine di espandersi oltre il contneitore al suo hover
     position: relative;   
 }
 
+.iframe-extend{
+    height: 475px;
+    width: 845px;
+
+}
+
 iframe, img{
-    max-width: 100%;
+    width: 100%;
 }
 
 .yt-button{
@@ -66,10 +78,20 @@ iframe, img{
 // IMG
 #cover-image, .overlay{
     border-radius: 5px;
-    transition: transform 0.5s ease; // Aggiungo una animazione smooth di durata 0.5s
+    transition: transform 1s ease; // Aggiungo una animazione smooth di durata 0.5s
 }
 
 .overlay:hover{
     transform: scale(1.1); // zommo l'immagine rispetto alla sua proporzione originale di 1.1 all'hover
 }
+
+.close-button {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    font-size: 2rem;
+    color: white;
+    cursor: pointer;}
 </style>
+
+<!-- MADE BY ANDREA GULLI -->
