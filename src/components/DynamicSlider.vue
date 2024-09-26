972904a58918ad691afabeaea7440d324a3e0023
Andrea Gulli <andreagulli11@gmail.com>
2024-09-26 11:24:54 +0200

    complete components IframeVideos (animaton & improvement yet do to)

--- src/components/DynamicSlider.vue
+++ src/components/DynamicSlider.vue
@@ -11,16 +11,17 @@ export default {
 <template>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" style="background-color: red;"></li> <!-- Colore rosso -->
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" style="background-color: red;"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2" style="background-color: red;"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3" style="background-color: red;"></li> <!-- Aggiunto per la quarta slide -->
         </ol>
-        <div class="carousel-inner bg-black">
+        <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="carousel-card">
-                <div class="title">High level of efficiency and scientific teaching methods</div>
+                <h4 class="title">High level of efficiency and scientific teaching methods</h4>
                 <p class="text">
-                  I am free t learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.	
+                  I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.	
                 </p>
                 <div class="img-wrapper d-flex justify-content-start align-items-center">
                   <img src="../assets/img/testimonial-avata-02.jpg" alt="testimonial-avatar-01">
@@ -33,8 +34,10 @@ export default {
           </div>
           <div class="carousel-item">
             <div class="carousel-card">
-              <div class="title">High level of efficiency and scientific teaching methods</div>
-              <p class="carousel-text">					I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.				</p>
+              <h4 class="title">High level of efficiency and scientific teaching methods</h4>
+              <p class="text">					
+                  I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.				
+              </p>
               <div class="carousel-img">
                   <img src="../assets/img/testimonial-avata-01.jpg" alt="testimonial-avatar-01">
                   <h3 class="img-title">Mina Hollace</h3>
@@ -44,8 +47,10 @@ export default {
           </div>
           <div class="carousel-item">
             <div class="carousel-card">
-              <div class="title">High level of efficiency and scientific teaching methods</div>
-              <p class="text">					I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.				</p>
+              <h4 class="title">High level of efficiency and scientific teaching methods</h4>
+              <p class="text">					
+                  I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.				
+              </p>
               <img src="../assets/img/testimonial-avata-03.jpg" alt="testimonial-avatar-01">
               <div class="img-content">
                   <h3 class="img-title">Mina Hollace</h3>
@@ -53,62 +58,78 @@ export default {
               </div>
             </div>
           </div>
+          <div class="carousel-item">
+            <div class="carousel-card">
+              <h4 class="title">High level of efficiency and scientific teaching methods</h4>
+              <p class="text">					
+                  I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.				
+              </p>
+              <img src="../assets/img/testimonial-avata-02.jpg" alt="testimonial-avatar-01">
+              <div class="img-content">
+                  <h3 class="img-title">Mina Hollace</h3>
+                  <p class="img-text">/ Freelancer</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-      
+    </div>
 </template>
 
 <style lang="scss" scoped>
+*{
+  font-family: 'Roboto';
+}
 // CARD CAROUSEL
-.carousel-card{
+.carousel-card {
+  margin-top: 50px;
     width: 700px;
     height: 350px;
-    border: 1px solid black;
-    padding: 40px 50px;
+    border: 1px solid lightgray;
+    padding: 40px; 
+    border-radius: 12px;
 }
 
-.carousel-card .title{
+.carousel-card .title {
   font-size: 20px;
   color: #3F3A64;
+  text-align: left; 
 }
 
-.carousel-card .text{
-  margin-top: 15px;
+.carousel-card .text {
+  margin-top: 25px;
+  margin-bottom: 25px;
   font-size: 18px;
   font-weight: 500;
   color: #696969;
+  white-space: pre-line; 
+  text-align: left; 
 }
 
-.carousel-card img{
+.carousel-card img {
   height: 70px;
   width: 70px;
   max-width: 100%;
   border-radius: 50%;
 }
 
-.carousel-img-content{
+.carousel-img-content {
   margin-left: 25px;
   flex-direction: column;
   align-items: start;
+  justify-content: center;
 }
 
-.carousel-card .carousel-img-content .img-title{
+.carousel-card .carousel-img-content .img-title {
   font-size: 15px;
   color: #3F3A64;
   text-transform: uppercase;
   letter-spacing: 1px;
+  margin-bottom: 12px;
 }
 
-.carousel-card .carousel-img-content .img-text{
+.carousel-card .carousel-img-content .img-text {
   font-size: 14px;
   color: #7e7e7e;
+  margin-bottom: 0;
 }
 </style>
